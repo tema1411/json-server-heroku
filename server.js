4df38bb5e8c3d240529ca56af42442520a1ff74d
artem ilchenko <ailchenko@dynamo-ny.com>
2022-03-10 16:25:01 +0200

    add mock data

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 server.use(middlewares);
+server.use(jsonServer.rewriter({
+    "api/recommendations:": "/recommendations",
+    "/api/regions/emissions-factors": "/emissions",
+    "/api/*": "/$1",
+    "/api": "/footprint"
+}))
 server.use(router);
 
+
 server.listen(port);
+
+console.log(`Started on port: ${port}`)
